7db3d5273b99b0cc2918394832411e3068bf8561
Magnus Bjerke Vik <mbvett@gmail.com>
2016-10-28 14:41:04 +0200

    Add PHPUnit setup to provision script

--- provision.sh
+++ provision.sh
@@ -96,6 +96,17 @@ php admin/cli/install.php \
 	--agree-license \
 	--non-interactive
 chown www-data:www-data -R /var/www/moodle
+
+echo "Setting up PHPUnit..."
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+sudo mv phpunit.phar /usr/local/bin/phpunit
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar install
+
 echo "Restarting Apache..."
 service apache2 restart
 cat <<EOF
